46baa8750bb7964fcfe376e52d9e27f1481fb598
mtaylorpdx <taylor.matt@protonmail.com>
2020-01-24 14:13:08 -0800

    replace instances of 2 and 3 with boop and dave

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,16 @@
 var dave = function(number) {
   var result = [];
   for (var currentNumber = 0; currentNumber <= number; currentNumber +=1) {
-    if (currentNumber.toString().includes("1")) {
+
+    if (currentNumber.toString().includes("3")) {
+      console.log(currentNumber)
+      result.push("I'm sorry, Dave. I'm afraid I can't do that.");
+
+    } else if (currentNumber.toString().includes("2")) {
+        console.log(currentNumber)
+        result.push("Boop!");
+
+    } else if (currentNumber.toString().includes("1")) {
       console.log(currentNumber)
       result.push("Beep!");
   } else {
