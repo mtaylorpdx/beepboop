9b6c456e11624086501ebd1495f31a9a1fd3c7f6
mtaylorpdx <taylor.matt@protonmail.com>
2020-01-24 12:31:03 -0800

    replace single instances of 1 with beep

--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,13 @@
 var dave = function(number) {
   result = [];
   for (var currentNumber = 1; currentNumber <= number; currentNumber +=1) {
+    if (currentNumber === 1) {
+      result.push("Beep!");
+  } else {
     result.push(currentNumber + " ");
-
+  }
   }
   return result;
-  console.log(result);
 };
 
 
@@ -24,33 +26,3 @@ $(document).ready(function() {
     $("#output").append(result);
   });
 });
-
-
-
-
-// // business logic
-
-// var dave = function(array) {
-
-//   for (var currentNumber = 1; currentNumber <= array[0]; currentNumber +=1) {
-//     return currentNumber
-//   }
-
-
-//   console.log(array);
-// }
-
-
-
-// //user interface logic
-
-// $(document).ready(function() {
-//   $("form#input").submit(function(event) {
-//     event.preventDefault();
-
-//     var number = $("input#inputNumber").val();
-//     var array = number.split(" ");
-//     var result = dave(array);
-//     $("#output").text(result);
-//   });
-// });
