0caf6a40851738e7c51b0b52caae608cf4ed97e3
mtaylorpdx <taylor.matt@protonmail.com>
2020-01-24 15:35:09 -0800

    add reset button

--- js/scripts.js
+++ js/scripts.js
@@ -3,21 +3,29 @@
 var dave = function(number) {
   var result = [];
   for (var currentNumber = 0; currentNumber <= number; currentNumber +=1) {
-
     if (currentNumber.toString().includes("3")) {
       result.push("I'm sorry, Dave. I'm afraid I can't do that. ");
-
-    } else if (currentNumber.toString().includes("2 ")) {
-        result.push("Boop!");
-
-    } else if (currentNumber.toString().includes("1 ")) {
-      result.push("Beep!");
-  } else {
-    result.push(currentNumber + " ");
-  }
+    } else if (currentNumber.toString().includes("2")) {
+        result.push("Boop! ");
+    } else if (currentNumber.toString().includes("1")) {
+        result.push("Beep! ");
+    } else if (currentNumber.toString().includes(NaN)) {
+        result.push("Please enter a number, Dave. ")
+    } else {
+      result.push(currentNumber + " ");
+    }
   }
   return result;
 };
+
+function stringCheck(a, obj) {
+  for (var i = 0; i< a.length; i++) {
+      if (a[i] === obj) {
+        return true;
+      }
+ } 
+ return false;
+}
 
 //user interface logic
 
@@ -26,5 +34,41 @@ $(document).ready(function() {
     event.preventDefault();
     var number = parseInt($("input#inputNumber").val());
     $("#output").append(dave(number));
+
+    $("#reset").click(function() {
+      document.location.reload(true);
+    });
   });
 });
+
+
+
+
+
+
+
+// var dave = function(number) {
+//   var result = [];
+//   for (var currentNumber = 0; currentNumber <= number; currentNumber +=1) {
+
+//     if (currentNumber.toString().includes("3")) {
+//       result.push("I'm sorry, Dave. I'm afraid I can't do that. ");
+//     } else if (currentNumber.toString().includes("2 ")) {
+//         result.push("Boop!");
+//     } else if (currentNumber.toString().includes("1 ")) {
+//         result.push("Beep!");
+//     } else {
+//       result.push(currentNumber + " ");
+//     }
+//   }
+//   return result;
+// };
+
+// function stringCheck(a, obj) {
+//   for (var i = 0; i< a.length; i++) {
+//       if (a[i] === obj) {
+//         return true;
+//       }
+//  } 
+//  return false;
+// }
