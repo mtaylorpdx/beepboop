7e4da9c5d78013a35eb452af198818cc8c6e7d84
mtaylorpdx <taylor.matt@protonmail.com>
2020-01-24 15:56:15 -0800

    simplify variable names

--- js/scripts.js
+++ js/scripts.js
@@ -2,23 +2,22 @@
 
 var dave = function(number) {
   var result = [];
-  for (var currentNumber = 0; currentNumber <= number; currentNumber +=1) {
-    if (currentNumber.toString().includes("3")) {
+  for (var i = 0; i <= number; i +=1) {
+    if (i.toString().includes("3")) {
       result.push("I'm sorry, Dave. I'm afraid I can't do that. ");
-    } else if (currentNumber.toString().includes("2")) {
+    } else if (i.toString().includes("2")) {
         result.push("Boop! ");
-    } else if (currentNumber.toString().includes("1")) {
+    } else if (i.toString().includes("1")) {
         result.push("Beep! ");
-    } else if (currentNumber.toString().includes(NaN)) {
-        result.push("Please enter a number, Dave. ")
     } else {
-      result.push(currentNumber + " ");
+      result.push(i + " ");
     }
   }
   return result;
 };
 
 function stringCheck(a, obj) {
+
   for (var i = 0; i< a.length; i++) {
       if (a[i] === obj) {
         return true;
