949e8ca593d64a7f2d0e142e8158da8597cf9353
mtaylorpdx <taylor.matt@protonmail.com>
2020-01-24 16:33:19 -0800

    stylesheet changes

--- js/scripts.js
+++ js/scripts.js
@@ -16,6 +16,7 @@ var dave = function(number) {
   return result;
 };
 
+
 function stringCheck(number, obj) {
   var strNumber = number.toString();
   for (var i = 0; i< strNumber.length; i++) {
@@ -33,40 +34,8 @@ $(document).ready(function() {
     event.preventDefault();
     var number = parseInt($("input#inputNumber").val());
     $("#output").append(dave(number));
-
     $("#reset").click(function() {
       document.location.reload(true);
     });
   });
 });
-
-
-
-
-
-
-// var dave = function(number) {
-//   var result = [];
-//   for (var i = 0; i <= number; i +=1) {
-//     if (i.toString().includes("3")) {
-//       result.push("I'm sorry, Dave. I'm afraid I can't do that. ");
-//     } else if (i.toString().includes("2")) {
-//         result.push("Boop! ");
-//     } else if (i.toString().includes("1")) {
-//         result.push("Beep! ");
-//     } else {
-//       result.push(i + " ");
-//     }
-//   }
-//   return result;
-// };
-
-// function stringCheck(a, obj) {
-
-//   for (var i = 0; i< a.length; i++) {
-//       if (a[i] === obj) {
-//         return true;
-//       }
-//  } 
-//  return false;
-// }
